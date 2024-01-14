a1f4472643bf6d299798a0e5899af42a913143e7
xRetry <marcoschwarz.at@gmail.com>
2024-01-14 11:34:10 +0100

    Change state management to struct field

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use anyhow::{Result, anyhow};
 
-struct Peripherals {
+struct Gpio0 {
 
+}
+
+struct Gpio1 {
+
+}
+
+struct Peripherals {
+    gpio0: Gpio0,
+    gpio1: Gpio1,
 }
 
 impl Peripherals {
@@ -14,17 +23,17 @@ impl Peripherals {
     }
 }
 
-type FnInit = Box<dyn FnMut(i32) -> Result<()>>;
-type FnExec = Box<dyn Fn(i32) -> Result<f64>>;
+type FnInit<T> = Box<dyn FnMut(i32, &T) -> Result<()>>;
+type FnExec<T> = Box<dyn Fn(i32, &T) -> Result<f64>>;
 
-struct PinOp {
-    fn_init: FnInit,
-    fn_op: FnExec,
+struct PinOp<T> {
+    fn_init: FnInit<T>,
+    fn_op: FnExec<T>,
     allowed: Vec<i32>,
 }
 
-impl PinOp {
-    fn new(fn_init: FnInit, fn_op: FnExec, allowed: Vec<i32>) -> Self {
+impl<T> PinOp<T> {
+    fn new(fn_init: FnInit<T>, fn_op: FnExec<T>, allowed: Vec<i32>) -> Self {
         return PinOp{
             fn_init,
             fn_op,
@@ -33,29 +42,36 @@ impl PinOp {
     }
 }
 
-struct PinInterface<'a> {
-    pin_ops: Vec<PinOp>,
-    active: Vec<&'a FnExec>,
+struct PinInterface<T> {
+    pin_ops: Vec<PinOp<T>>,
+    active: Vec<i32>,
+    state: T,
 }
 
-impl<'a> PinInterface<'a> {
-    pub fn new(pin_ops: Vec<PinOp>) -> Self {
-        let mut active: Vec<&FnExec> = Vec::new();
-        for _ in 0..10 {
-            active.push(&pin_ops[0].fn_op);
-        }
-        return Self{
+impl<T> PinInterface<T> {
+    pub fn new(pin_ops: Vec<PinOp<T>>, state: T) -> Self {
+        let mut interface = Self{
             pin_ops,
-            active
+            active: Vec::new(),
+            state,
         };
+
+        for _ in 0..10 {
+            interface.active.push(0);
+        }
+        return interface;
     }
 
     pub fn exec_op(&self, pin_nr: i32) -> Result<f64> {
-        let Some(fn_op) = self.active.get(pin_nr as usize) else {
+        let Some(idx_fn_op) = self.active.get(pin_nr as usize) else {
             return Err(anyhow!("Invalid pin number"));
         };
 
-        return fn_op(pin_nr);
+        let Some(fn_op) = self.pin_ops.get(*idx_fn_op as usize) else {
+            return Err(anyhow!("Invalid pin number"));
+        };
+
+        return (*fn_op.fn_op)(pin_nr, &self.state);
     }
 
     pub fn init_op(&mut self, pin_nr: i32, op_nr: i32) -> Result<()> {
@@ -67,40 +83,34 @@ impl<'a> PinInterface<'a> {
             return Err(anyhow!("Invalid pin number"));
         }
 
-        (pin_op.fn_init)(pin_nr)?;
-        self.active[pin_nr as usize] = &pin_op.fn_op;
+        (pin_op.fn_init)(pin_nr, &self.state)?;
+        let Some(idx_fn_op) = self.active.get_mut(pin_nr as usize) else {
+            return Err(anyhow!("Invalid pin number"));
+        };
+
+        *idx_fn_op = op_nr;
 
         return Ok(());
     }
 }
 
-fn disabled(pin_nr: i32) -> Result<f64> {
+fn disabled(pin_nr: i32, per: &Peripherals) -> Result<f64> {
     return Ok(10.);
 }
 
-fn init_digital_read(pin_nr: i32) -> Result<()> {
+fn init_digital_read(pin_nr: i32, per: &Peripherals) -> Result<()> {
     return Ok(());
 }
 
-fn digital_read(pin_nr: i32) -> Result<f64> {
+fn digital_read(pin_nr: i32, per: &Peripherals) -> Result<f64> {
     return Ok(10.);
 }
 
 fn main() {
-    let mut per = Peripherals{};
-
-    let fn_init_digital_read = |_: i32| {
-        per.init();
-        Ok(())
-    };
-
-    let fn_digital_read = |_: i32| {
-        per.read();
-        let ret: f64 = 10.;
-        return Ok(ret);
-    };
+    let per = Peripherals{ gpio0: Gpio0{}, gpio1: Gpio1{} };
 
     let interface = PinInterface::new(
-        vec![PinOp::new(Box::new(fn_init_digital_read), Box::new(fn_digital_read), vec![1, 2, 3, 10])],
+        vec![PinOp::new(Box::new(init_digital_read), Box::new(digital_read), vec![1, 2, 3, 10])],
+        per,
     );
 }
